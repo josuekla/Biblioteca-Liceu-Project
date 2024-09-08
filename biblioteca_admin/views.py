c94b956a43074a6dad7153eb93d982239a7469a3
josuekla <josueklayslerjk@gmail.com>
2024-09-08 11:36:28 -0300

    Conseguindo inprementar um template no app main

--- biblioteca_admin/views.py
+++ biblioteca_admin/views.py
@@ -4,11 +4,12 @@ def biblioteca_admin(request):
     print("Admin Funcionando!")
 
 
-    # context = {
-    #     'title' : 'Texto modificado pelo josué'
-    # }
+    context = {
+        'title' : 'Texto modificado pelo josué',
+    }
 
     return render(
         request,
         'biblioteca_admin/home.html',
+        context,
     )
