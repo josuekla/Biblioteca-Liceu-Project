e24238e873b22eeb428fe128f837f34058f75c6c
josuekla <josueklayslerjk@gmail.com>
2024-09-07 21:25:49 -0300

    Criando os primeiros passos nas view.py dos apps

--- biblioteca_admin/views.py
+++ biblioteca_admin/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def bilioteca_admin(request):
+    print("Admin Funcionando!")
+
--- biblioteca_virtual/settings.py
+++ biblioteca_virtual/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biblioteca_admin',
+    'estudante',
+    'visitante',
+
+    
 ]
 
 MIDDLEWARE = [
